05a45e4a6b400220a00fb666ec9d6e46f8fb542e
Vova <strikerxp1990@gmail.com>
2023-05-03 10:42:25 +0300

    First page almost ready

--- src/components/UI/components.js
+++ src/components/UI/components.js
@@ -1,9 +1,5 @@
 /** @format */
 
-import MyButton from "@/components/UI/MyButton.vue";
-import MyInput from "@/components/UI/MyInput.vue";
-import MyDialogue from "@/components/UI/MyDialogue.vue";
-import MySelect from "@/components/UI/MySelect.vue";
-import MyTextarea from "@/components/UI/MyTextarea.vue";
+import StartTestButton from "@/components/UI/NextButton.vue";
 
-export default [MyButton, MyInput, MyDialogue, MySelect, MyTextarea];
+export default [StartTestButton];
